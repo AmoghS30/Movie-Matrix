74de19e505778a7d0cec1c24866286c6d6b5ec04
Amogh S <as7636688@gmail.com>
2025-05-19 06:51:05 +0530

    Developed Search Screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -12,11 +12,7 @@ const TabIcon = ({ focused, icon, title }: any) => {
           source={images.highlight}
           className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
         >
-          <Image
-            source={icons.home}
-            tintColor={"#151312"}
-            className="size-5 "
-          />
+          <Image source={icon} tintColor={"#151312"} className="size-5 " />
           <Text className="text-secondary font-semibold text-base ml-2">
             {title}
           </Text>
